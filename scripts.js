c76ef887fda32719a8d2812c87cd48142e112194
davisllv <davisllvsnt57@gmail.com>
2022-03-11 18:43:03 -0300

    creating the function who open and close the modal

--- scripts.js
+++ scripts.js
@@ -7,3 +7,13 @@ const closeModal = document.getElementById("closeModal");
 openModal.onclick = function () {
   modalOverlay.style.display = "block";
 };
+
+closeModal.onclick = () => {
+  modalOverlay.style.display = "none";
+};
+
+window.onclick = (ev) => {
+  if (ev.target === modalOverlay) {
+    modalOverlay.style.display = "none";
+  }
+};
