c398f5bfbc89cf0eef50f821d990cc8a85b04949
davisllv <davisllvsnt57@gmail.com>
2022-03-14 17:33:02 -0300

    fixing the data

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 const modalOverlay = document.getElementsByClassName("modal-overlay")[0];
 
-const saveModal = document.getElementsByClassName("save")[0];
-
 const Modal = {
   ModalOpen() {
     modalOverlay.style.display = "block";
   },
 
-  ModalClose() {
+  ModalClose(ev) {
+    ev.preventDefault();
     modalOverlay.style.display = "none";
   },
 };
@@ -27,57 +26,44 @@ const totalWhidrawAmountCard =
 
 const transactions = [];
 
-saveModal.onclick = (ev) => {
-  ev.preventDefault();
-  const data = Array.from(
-    document.querySelectorAll("#transaction-form input")
-  ).reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {});
-  const amount = Number(data.number);
+const Transaction = {
+  SaveTransaction(ev) {
+    ev.preventDefault();
 
-  totalAmount += amount;
+    const data = Array.from(
+      document.querySelectorAll("#transaction-form input")
+    ).reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {});
 
-  if (amount < 0) {
-    whidrawAmount += amount;
-  } else {
-    depositAmount += amount;
-  }
-
-  totalDepositCard.textContent = depositAmount.toLocaleString("pt-br", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  totalWhidrawAmountCard.textContent = whidrawAmount.toLocaleString("pt-br", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  totalAmountCard.textContent = totalAmount.toLocaleString("pt-br", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  modalOverlay.style.display = "none";
-  data.id = Math.round(Math.random() * 100 + 1);
-  data.image = "./assets/minus.svg";
-
-  transactions.push(data);
-
-  let tbody = document.getElementById("tbody");
-  transactions.forEach((item, index) => {
-    let tr = tbody.insertRow(index);
-
-    let td_description = tr.insertCell();
-    let td_amount = tr.insertCell();
-    let td_date = tr.insertCell();
-    let td_actions = tr.insertCell();
-
-    if (item.number < 0) {
-      td_amount.classList.add("whitdraw");
-      td_amount.innerText = Number(item.number).toLocaleString("pt-br", {
-        style: "currency",
-        currency: "BRL",
-      });
+    if (!data.description || !data.number || !data.date) {
+      alert(`Set all the data`);
+      return;
     }
-  });
+
+    transactions.push(data);
+
+    const amount = Number(data.number);
+
+    totalAmount += amount;
+
+    if (amount < 0) {
+      whidrawAmount += amount;
+    } else {
+      depositAmount += amount;
+    }
+
+    totalDepositCard.textContent = depositAmount.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    totalWhidrawAmountCard.textContent = whidrawAmount.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    totalAmountCard.textContent = totalAmount.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  },
 };
